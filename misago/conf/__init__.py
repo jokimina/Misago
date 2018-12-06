90948b131d0b07c73e07e045208ab9b8e857fb6c
rafalp <kontakt@rpiton.com>
2018-12-06 22:17:08 +0100

    Small code cleanups before removing global state from create_user

--- misago/conf/__init__.py
+++ misago/conf/__init__.py
@@ -3,3 +3,7 @@ from .gateway import settings, db_settings  # noqa
 CACHE_NAME = "settings"
 
 default_app_config = 'misago.conf.apps.MisagoConfConfig'
+
+
+# Feature flag for easy changing between old and new settings mechanism
+ENABLE_GLOBAL_STATE = True  # FIXME: delete after API changes
